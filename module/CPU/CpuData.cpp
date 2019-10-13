e0030f0d23bf4dfed8e0402d0443b061835958da
jongwuner <jklh0202@naver.com>
2019-10-13 17:21:25 +0900

    Update CpuData.cpp
    # CpuData.cpp
    - CpuSpeed : 자료형 DWORD로 변경.
    - InitReg 추가 : 레지스트리 초기화는 이 메서드에서 따로 진행
    - GetCpuSpeed 추가
    - GetWorkTime 추가

--- module/CPU/CpuData.cpp
+++ module/CPU/CpuData.cpp
@@ -6,6 +6,7 @@
 #include "TCHAR.h"
 #include "pdh.h"
 #include "atlstr.h"
+#include<time.h>
 
 using namespace std;
 
@@ -20,29 +21,37 @@ private:
 	SYSTEM_INFO SysInfo;
 	wchar_t CpuName[255];//cpu 이름 O
 	double CpuUsage;//cpu 사용량 O
-	double Speed;	//cpu 속도 
+	DWORD CpuSpeed;	//cpu 속도 O 기본속도
 	int ProcessNum;	//process 갯수 O
-	int ThreadNum;		//thread 갯수 O 
+	int ThreadNum;	//thread 갯수 O 
 	int HandleNum;	//핸들러 개수(?)
-	time_t WorkTime; // 작동된 시간
+	time_t BeginTime, WorkTime; // 작동된 시간 O
 
-	double Speed_basic;	// 기본속도(?)
+
 	int SocketNum;	//소켓 갯수(?)
 	int CoreNum;	//코어 갯수(?)
-	int logicProc;  //논리 프로세서(?)
-	bool isVirt;	//가상화 사용여부
-	double L1Cach;	//L1 캐시 용량
-	double L2Cach;  //L2 캐시 용량
-	double L3Cach;	//L3 캐시 용량
+	int logicProc;  //논리 프로세서(O)
+	bool isVirt;	//가상화 사용여부(?)
+	double L1Cach;	//L1 캐시 용량(?)
+	double L2Cach;  //L2 캐시 용량(?)
+	double L3Cach;	//L3 캐시 용량(?)
+
+	HKEY hKey;
+	DWORD cname_size;
+	DWORD cspeed_size;
 
 public:
 	CpuData() {
+		BeginTime = clock();
+		
 		//init 생성자
 		GetSystemInfo(&SysInfo);
 		//cpu 타입, 
+		InitReg();
 		InitCPUName();
 		InitThreadNum();
 		InitCPUUsage();
+
 	}
 
 	void InitThreadNum() {
@@ -54,17 +63,16 @@ public:
 		PdhAddCounter(cpuQuery, L"\\Processor(_Total)\\% Processor Time", NULL, &cpuTotal);
 		PdhCollectQueryData(cpuQuery);
 	}
+	void InitReg() {
+		cname_size = sizeof(CpuName);
+		cspeed_size = sizeof(CpuSpeed);
 
-	void InitCPUName() {
-		HKEY hKey;
-		int i = 0;
-		long result = 0;
-		DWORD c_size = sizeof(CpuName);
-
-		//레지스트리를 조사하여 프로세서의 모델명을 얻어냅니다.
 		RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Hardware\\Description\\System\\CentralProcessor\\0", 0, KEY_QUERY_VALUE, &hKey);
-		RegQueryValueEx(hKey, L"ProcessorNameString", NULL, NULL, (LPBYTE)CpuName, &c_size);
+		RegQueryValueEx(hKey, L"ProcessorNameString", NULL, NULL, (LPBYTE)CpuName, &cname_size);
+		RegQueryValueEx(hKey, L"~MHz", NULL, NULL, (LPBYTE)&CpuSpeed, &cspeed_size); // CPU Speed
 		RegCloseKey(hKey);
+	}
+	void InitCPUName() {
 
 		//GetSystemInfo 함수를 이용해 논리적 코어 개수를 얻어냅니다.
 		wchar_t num[8];
@@ -73,7 +81,7 @@ public:
 		swprintf(num, 8, L" * %d", systemInfo.dwNumberOfProcessors);
 		wcscat_s(CpuName, 100, num);
 	}
-	
+
 	double GetCpuUsage() {
 		PDH_FMT_COUNTERVALUE counterVal;
 
@@ -81,17 +89,31 @@ public:
 		PdhGetFormattedCounterValue(cpuTotal, PDH_FMT_DOUBLE, NULL, &counterVal);
 		return counterVal.doubleValue;
 	}
-	
+
 	wstring GetCpuName() {
 		return CpuName;
+	}
+	DWORD GetCpuSpeed() {
+		return CpuSpeed / 1000.0;
+	}
+	time_t GetWorkTime() {
+		WorkTime = clock();
+		return (WorkTime - BeginTime) / 1000.0;
 	}
 };
 
 
 int main() {
 	CpuData c;
-	
-	wcout << "CPU Name : "<<c.GetCpuName()<<"\n";
+
+	wcout << "CPU Name : " << c.GetCpuName() << "\n";
+	while (true) {
+		cout << "CPU Usage : " << c.GetCpuUsage() << "% \n";
+		cout << "CPU Speed : " << c.GetCpuSpeed() << "GHz \n";
+		cout << "CPU WorkTime : " << c.GetWorkTime() << "Sec \n";
+		cout << "Sleep !\n\n";
+		Sleep(1000);
+	}
 
 	return 0;
 }
