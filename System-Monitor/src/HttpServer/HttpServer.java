96f62fc971e84e8d235fe87741d909f5f5fc970f
Favian <damin8@naver.com>
2019-12-23 18:25:11 +0900

    update
    사용 언어는 Java로 정했고
    
    Spring Server와의 통신을 위해
    
    HttpServer 구현 했습니다.
    
    주석처리하거나 설명같은거 써놨는데
    
    거기에다가 예약어 넣으면 될 듯 합니다.
    
    To do
    
    System-Monitor의 이미 있는 기능들 Java로 구현하기
    
    Swing 활용해서 GUI 짜기

---
+++ System-Monitor/src/HttpServer/HttpServer.java
@@ -0,0 +1,38 @@
+package HttpServer;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class HttpServer { // Main에서 생성
+
+	private Socket c_socket = null;
+
+	public HttpServer() {
+		try {
+			ServerSocket listener
+		= new ServerSocket(8888); // 8080 포트
+		while(true) { // 프로그램 종료될 떄 까지 실행
+			System.out.println("Waiting...");
+			c_socket = listener.accept(); // Block 메서드 (Client가 들어올 때 까지 멈춰있음)
+			System.out.println("Accept!");
+			SendData.getSendData().setSocket(c_socket);
+			ReceiveThread receiveThread = new ReceiveThread();
+			receiveThread.setSocket(c_socket);
+			receiveThread.start(); // 비동기 Receive
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
