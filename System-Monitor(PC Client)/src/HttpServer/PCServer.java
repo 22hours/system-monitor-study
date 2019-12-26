a9eda10986c68f6aa8afa69a920e0279a5e773c0
Favian <damin8@naver.com>
2019-12-26 20:26:55 +0900

    update
    http 통신으로 업데이트
    
    테스트 진행 결과
    
    1. 언제든 request 보내면 내가 설정해놓은 response 받음(비동기)
    
    2. 블럭 메서드가 없어서 request가 없어도 바로바로 다른 기능 수행 가능
    
    3. 통신 규약을 정해야 할 듯
    
    to do
    
    thread safe
    
    여러가지 기능
    
    gui

---
+++ System-Monitor(PC Client)/src/HttpServer/PCServer.java
@@ -0,0 +1,25 @@
+package HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class PCServer { 
+	
+	public PCServer() {
+		try {
+			HttpServer server = HttpServer.create(new InetSocketAddress(8888),0);
+					//8888포트, "/22hours" 컨텍스트로 서비스
+			server.createContext("/22hours", new MyHandler());
+			server.setExecutor(null);
+			server.start();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
