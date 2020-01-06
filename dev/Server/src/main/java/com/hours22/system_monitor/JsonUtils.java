67d238f06e7731b973f0152a3059a7673b43a149
jongwuner <jklh0202@naver.com>
2020-01-07 07:57:13 +0900

    update Server/JsonToJson
    # Server/JsonToJson (미완성 커밋)
     : GET, POST 둘다
     : JsonToObj -> ObjToJson 구조로 통신
    - GET LocalLoad(JsonToObj)
    - GET HttpResponse(ObjToJson)
    - POST HttpRequest(JsonToObj)
    - POST LocalSave(ObjToJson)

--- dev/Server/src/main/java/com/hours22/system_monitor/JsonUtils.java
+++ dev/Server/src/main/java/com/hours22/system_monitor/JsonUtils.java
@@ -1,76 +1,105 @@
 package com.hours22.system_monitor;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper; 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 
 public class JsonUtils { 
-    private JsonUtils(){} 
-    public static Map stringToJsonMap(String json){ 
-        return stringToObject(json, HashMap.class); 
-    } 
-    public static Object stringToJsonClass(String json, Class clazz){ 
-        return stringToObject(json, clazz); 
-    } 
-    public static <T> T stringToObject(String jsonString, Class<T> valueType){ 
-        try { 
-            return new ObjectMapper().readValue(jsonString, valueType); 
-        } 
-        catch (JsonParseException e) { 
-            e.printStackTrace(); 
-        } 
-        catch (JsonMappingException e) { 
-            e.printStackTrace(); 
-        } 
-        catch (IOException e) { 
-            e.printStackTrace(); 
-        } 
-        return null; 
-    } 
-    public static String jsonToString(Object jsonObject){ 
-        return objectToString(jsonObject); 
-    } 
-    
-    public static String gainJsonToString(Object jsonObject){ 
-        return gainJsonToString(jsonObject, true); 
-    } 
-    public static String gainJsonToString(Object jsonObject, boolean resultAddFlag){ 
-        if(resultAddFlag){ 
-            Map m = new HashMap(); 
-            m.put("result", jsonObject); 
-            return objectToString(m); 
-        }
-        else{ 
-            return objectToString(jsonObject); 
-        } 
-    } 
-    public static String objectToString(Object json) { 
-        ObjectMapper om = new ObjectMapper(); 
-        try { 
-            return om.writeValueAsString(json); 
-        } 
-        catch (JsonGenerationException e) { 
-            e.printStackTrace(); 
-        } 
-        catch (JsonMappingException e) {
-            e.printStackTrace(); 
-        } 
-        catch (IOException e) { 
-            e.printStackTrace(); 
-        } 
-        return ""; 
-    } 
-    public static String callbackObjectToString(String callback , Object json){ 
-        String reval = objectToString(json); 
-        
-        if(!"".equals(callback)){ 
-            reval = callback+"("+reval+")"; 
-        } 
-        return reval; 
-    } 
+	
+	// GET : Json->Obj (LocalLoad)
+	public static PC LocalLoad_JsonToObj(int idx) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		
+		try {
+			String dir = "pcs\\result"+idx+".json";
+			
+			PC tpc = mapper.readValue(new File(dir), PC.class);
+			return tpc;
+			
+		} catch(JsonGenerationException e){
+			e.printStackTrace();
+		} catch(JsonMappingException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return new PC(-1);
+	}
+	
+	// GET : Obj->Json(HttpResponse)
+	public static Map HttpResponse_ObjToJson(PC tpc) {
+		ObjectMapper mapper = new ObjectMapper();
+		
+		try {
+			//객체를 JSON 타입의 String으로 변환.
+			String jsonInString = mapper.writeValueAsString(tpc);
+			//객체를 JSON 타입의 String으로 변환 및 정렬.
+			Map<String, String> map = mapper.readValue(jsonInString, Map.class);
+			return map;
+			
+		}catch (JsonGenerationException e){
+			e.printStackTrace();
+		}catch (JsonMappingException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		Map<String, String> empty = new HashMap<String, String>();
+		return empty;
+	}
+
+	
+	
+	// POST : Json->Obj(HttpRequest)
+	public static PC HttpRequest_JsonToObj(int idx) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		
+		try {
+			String dir = "pcs\\result"+idx+".json";
+			
+			PC tpc = mapper.readValue(new File(dir), PC.class);
+			return tpc;
+			
+		} catch(JsonGenerationException e){
+			e.printStackTrace();
+		} catch(JsonMappingException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return new PC(-1);
+	}
+	
+	// POST : Obj->Json(LocalSave)
+	public static void LocalSave_ObjToJson(PC tpc) {
+		ObjectMapper mapper = new ObjectMapper();
+		
+		try {
+			// 객체를 JSON타입 파일로 변환.
+			String dir = "pcs\\result"+tpc.get_id()+".json";
+			mapper.writeValue(new File(dir), tpc);
+
+		}catch (JsonGenerationException e){
+			e.printStackTrace();
+		}catch (JsonMappingException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
