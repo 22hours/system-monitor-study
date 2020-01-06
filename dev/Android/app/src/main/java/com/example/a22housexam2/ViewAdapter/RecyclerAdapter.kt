ec32782f7e4479e5deba2678f17cecc0d638cd84
이정환 <46296754+winterlood@users.noreply.github.com>
2020-01-06 23:44:35 +0900

    #2 Android : make bottom navigation bar, make recycler view, make card view
    upgrade gradle version
    android.support.v7 -> androidx
    
    make bottom navigation bar,
    make recycler view,
    make card view
    
    make ViewHolder

---
+++ dev/Android/app/src/main/java/com/example/a22housexam2/ViewAdapter/RecyclerAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.a22housexam2.ViewAdapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter{
+
+}
---
+++ dev/Android/app/src/main/java/com/example/a22housexam2/ViewAdapter/ViewItem/Recycler_item.kt
@@ -0,0 +1,5 @@
+package com.example.a22housexam2.ViewAdapter.ViewItem
+
+data class ViewItem(
+    val title : String,
+    val imageURI : String)
