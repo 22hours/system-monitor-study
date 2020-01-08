805da28005720bf261cc06e3a1cca5b61873a437
jongwuner <jklh0202@naver.com>
2020-01-09 01:34:53 +0900

    update Success GET, POST with Redis
    # Success GET, POST with Redis
    - JsonToJson 일단 초안 완성
    -> 모듈별로 잘 정리,
    -> Json 변수가 중복되는 현상.
    -> GET에서 response 저렇게 둬도 되는지? 코드 참고!
    
    # CommonController.java
    - GetPcInstanceData : pc 1개의 모든 정보를 get할때 사용
    - PostPcInstanceData : pc 1개의 모든 정보를 post할때 사용
    - 일단 완성
    
    # MobileController.java
    -> 정환이 요구사항으로 추후 개발예정
    
    # DataService.java
    - 실질적인 Redis의 write/read가 이뤄지는 코드
    
    # GetSetService.java
    - 예제
    
    # PcData.java
    - entity class

--- dev/Server/src/main/java/com/hours22/system_monitor/Controller/CommonController.java
+++ dev/Server/src/main/java/com/hours22/system_monitor/Controller/CommonController.java
@@ -9,13 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hours22.system_monitor.DataService;
+import com.hours22.system_monitor.PcData;
 
 @Controller
 public class CommonController {
+	@Autowired
+	ObjectMapper ojm;
+	
 	@Autowired
 	DataService dss;
 	
@@ -34,16 +40,28 @@ public class CommonController {
 
 	@RequestMapping(value = "/pc/{id}", method = RequestMethod.GET)
 	public void GetPcInstanceData(HttpServletResponse response,  @PathVariable Long id) throws IOException {
-		// RedisLoad_JsonToObj();
-		// HttpResponse_ObjToJson();
 		System.out.println("Input : /pc/"+ id +" <- GET method ");
+		
+		// RedisLoad_JsonToObj();
+		PcData pc = dss.RedisLoad_JsonToObj(id.toString());
+		
+		// HttpResponse_ObjToJson();
+		String json = ojm.writeValueAsString(pc);
+		System.out.println(json);
+
+		response.getWriter().print(json);
 	}
 	
 	@RequestMapping(value = "/pc/{id}", method = RequestMethod.POST)
-	public void PostPcInstanceData(HttpServletRequest request,  Map<String, String> map) throws IOException {
+	public void PostPcInstanceData(HttpServletRequest request,  @RequestBody Map<String, String> map) throws IOException {
+		String json = ojm.writeValueAsString(map);
+		System.out.println("Input : "+ json +" <- POST method ");
+		
+		
 		// HttpRequest_JsonToObj();
-		// RedisSave_ObjToJson();
-		System.out.println("Input : "+ map +" <- POST method ");
+		PcData pc = ojm.readValue(json, PcData.class);
+			//System.out.println(pc.get_name());
+		dss.RedisSave_ObjToJson(pc);
 	}
 }
 
--- dev/Server/src/main/java/com/hours22/system_monitor/Controller/MobileController.java
+++ dev/Server/src/main/java/com/hours22/system_monitor/Controller/MobileController.java
@@ -1,5 +1,23 @@
 package com.hours22.system_monitor.Controller;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hours22.system_monitor.DataService;
+
+@Controller
 public class MobileController {
+	@Autowired
+	DataService dss;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public void GetTotalPcData(HttpServletResponse response) throws IOException {
+		// 모든 PC의 JSON 데이터 GET
+	}
 
 }
