1414af1f9e421c2b5fc41225b42b9c010ffa2977
Favian <damin8@naver.com>
2020-05-12 18:25:01 +0900

    update Service
    - c#으로 Service 만들기
    
    - Service 등록 = sc
    
    - Service로 프로그램 실행
    
    to do
    
    - Test
    - 꺼져 있는 프로그램 확인
    - 확인 후 재실행

---
+++ dev/Client(PC)/System_Monitor/System_Monitor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_Monitor
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ dev/Client(PC)/System_Monitor/System_Monitor/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_Monitor
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.Diagnostics.Process ps = new System.Diagnostics.Process();
+            ps.StartInfo.FileName = "SystemMonitor.exe";
+            ps.StartInfo.WorkingDirectory= "C:\\Users\\damin\\source\\repos\\System_Monitor\\System_Monitor\\bin\\Debug";
+            ps.Start();
+            ps.WaitForExit(1000);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
