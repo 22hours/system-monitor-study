f949a0fbb8d2822bfd040da92d70048b47ea972b
Favian <damin8@naver.com>
2020-05-19 01:45:21 +0900

    update Service
    - 시스템 종료될 때 종료하기

--- dev/Client(PC)/System_Monitor/System_Monitor/Service1.cs
+++ dev/Client(PC)/System_Monitor/System_Monitor/Service1.cs
@@ -75,7 +75,7 @@ namespace System_Monitor
                 foreach (Process p in allProc)
                 {
                     int pid = p.Id;
-                    if (GetProcessOwner(pid).Equals("System"))
+                    if (GetProcessOwner(pid).Equals("SYSTEM"))
                             p.Kill();
                 }
             }
